5ab975c0aacb1effad49f2f9565391e2d9c97460
Cappu-kyani <you@example.com>
2020-03-13 20:09:35 +0500

    cave

--- firstOcruglation/main.gd
+++ firstOcruglation/main.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const WIDTH = 100 # сколькл ячеек генерировать
 const HEIGHT = 30 # высота земной коры
+const HEIGHT_ = 100 # высота земной коры
+
 const CENTER_Y = 4 # середина мира по вертикали
 
 const generate_to_end = 30 # за сколко ячеек до конца начинать новую генерацию
@@ -17,13 +19,23 @@ func _ready():
 	randomize()
 	noise = OpenSimplexNoise.new()
 	noise.seed = randi()
-	noise.octaves = 4
-	noise.period = 15
-	noise.lacunarity = 2
+
+	noise.octaves = 1
+	noise.period = 8
+	noise.lacunarity = 1
 	noise.persistence = 0.25
-	generate_world()
+	generate_world_with_caves()
+
+func generate_world_with_caves():
+	for x in range(current_world_end, current_world_end + WIDTH):
+		for y in range(0, current_world_end + HEIGHT_):
+			noise.get_noise_2d(x, y)
+			var qwe = noise.get_noise_2d(x, y)
+			if qwe > 0:
+				$TileMap.set_cellv(Vector2(x, y), 0)
+
 	
-func generate_world():
+func generate_world_flat():
 	for x in range(current_world_end, current_world_end + WIDTH):
 		var delta_y = int(noise.get_noise_2d(x, CENTER_Y) * 10)
 		var y = CENTER_Y + delta_y
@@ -32,9 +44,9 @@ func generate_world():
 			$TileMap.set_cellv(Vector2(x, i), 1)
 	current_world_end += WIDTH
 
-func _process(delta):
-	delta_sum += delta
-	if delta_sum > 1:
-		if (current_world_end - $TileMap.world_to_map($player.position).x) < generate_to_end:
-			generate_world()
-		delta_sum = 0
+#func _process(delta):
+#	delta_sum += delta
+#	if delta_sum > 1:
+#		if (current_world_end - $TileMap.world_to_map($player.position).x) < generate_to_end:
+#			generate_world()
+#		delta_sum = 0
