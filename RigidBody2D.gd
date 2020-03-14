2a36723bf9336b17db1bac0798fced1e693f4862
Ваше Имя <you@example.com>
2020-03-14 06:24:57 -0400

    vrag

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -1,72 +1,78 @@
 extends KinematicBody2D
 
 # Angle in degrees towards either side that the player can consider "floor".
+# Member variables
+const GRAVITY = 500.0 # pixels/second/second
+var right = true
+# Angle in degrees towards either side that the player can consider "floor"
 const FLOOR_ANGLE_TOLERANCE = 40
 const WALK_FORCE = 600
 const WALK_MIN_SPEED = 10
 const WALK_MAX_SPEED = 200
 const STOP_FORCE = 1300
-const JUMP_SPEED = 400
+const JUMP_SPEED = 200
 const JUMP_MAX_AIRBORNE_TIME = 0.2
 
-const SLIDE_STOP_VELOCITY = 1.0 # Pixels/second
-const SLIDE_STOP_MIN_TRAVEL = 1.0 # Pixels
+const SLIDE_STOP_VELOCITY = 1.0 # one pixel/second
+const SLIDE_STOP_MIN_TRAVEL = 1.0 # one pixel
 
 var velocity = Vector2()
 var on_air_time = 100
 var jumping = false
+
 var prev_jump_pressed = false
 
-onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 10
 
 func _physics_process(delta):
+	# Create forces
+	var force = Vector2(0, GRAVITY)
 	
-	if self.position.x < 0:
-		self.position.x = 1
-		return
-	
-	var force = Vector2(0, gravity)
-	var walk = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var walk_left = !right
+	var walk_right = right
 	var jump = Input.is_action_pressed("jump")
-
-	if (velocity.x <= WALK_MIN_SPEED and velocity.x > -WALK_MAX_SPEED) or (velocity.x >= -WALK_MIN_SPEED and velocity.x < WALK_MAX_SPEED):
-		force.x += WALK_FORCE * walk
 	
-	if abs(walk) < 0.5:
+	var stop = true
+	
+	if walk_left:
+		if velocity.x <= WALK_MIN_SPEED and velocity.x > -WALK_MAX_SPEED:
+			force.x -= WALK_FORCE
+			stop = false
+	elif walk_right:
+		if velocity.x >= -WALK_MIN_SPEED and velocity.x < WALK_MAX_SPEED:
+			force.x += WALK_FORCE
+			stop = false
+	
+	if stop:
 		var vsign = sign(velocity.x)
 		var vlen = abs(velocity.x)
+		
 		vlen -= STOP_FORCE * delta
 		if vlen < 0:
 			vlen = 0
+		
 		velocity.x = vlen * vsign
-
-	# Integrate forces to velocity.
-	velocity += force * delta
-	# Integrate velocity into motion and move.
-	velocity = move_and_slide(velocity, Vector2.UP)
-
+	
+	# Integrate forces to velocity
+	velocity += force * delta	
+	# Integrate velocity into motion and move
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
 	if is_on_floor():
 		on_air_time = 0
-
+		
 	if jumping and velocity.y > 0:
-		# If falling, no longer jumping.
+		# If falling, no longer jumping
 		jumping = false
-
+	
 	if on_air_time < JUMP_MAX_AIRBORNE_TIME and jump and not prev_jump_pressed and not jumping:
 		# Jump must also be allowed to happen if the character left the floor a little bit ago.
 		# Makes controls more snappy.
 		velocity.y = -JUMP_SPEED
 		jumping = true
-
+	
 	on_air_time += delta
 	prev_jump_pressed = jump
-	
-	
-func ready():
-	add_to_group("Player")
-	
-	
-	
-	
-	
+
+func _on_Timer_timeout():
+	right = !right
 
